a3ec32c1c8c31dbe2f65733fe775a047f79340ea
Chris <chrisfan@jtechdigital.com>
2021-05-08 14:55:10 -0500

    add namedroutes & fix bug for fonts postion(wrong folder)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() {
@@ -30,6 +32,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      routes: {'/category-meals': (context) => CategoryMealsScreen()},
     );
   }
 }
